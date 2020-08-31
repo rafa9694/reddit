779c84f65bd9deeb605dae808565a1a40120d484
rafa9694 <rafa9694@gmail.com>
2020-08-31 10:13:57 -0300

    teste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                 <div>
                     <a class="navbar-brand" href="{{ url('/') }}">
                         <img src="/storage/system/reddit-24.png" alt="">
-                        <span class="title">reddit2</span>
+                        <span class="title">reddit</span>
                     </a>
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
